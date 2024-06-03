c3f6f6d151f7df1360e987d617b94c0872fdaa2d
homerduc <alexandre.duc00@orange.fr>
2024-06-03 12:30:29 +0200

    pb avec les unique_ptr

--- Schotten-Totten/Total.cpp
+++ Schotten-Totten/Total.cpp
@@ -4,7 +4,8 @@ Total::Total()
 {
     for (unsigned int itVal = 0; itVal != 9; itVal++) {
         for (unsigned int itCoul = 0; itCoul != 6; itCoul++) {
-            tas.push_back(make_unique<CarteClan>(itVal, itCoul));
+
+            tas.push_back(move(make_unique<CarteClan>(CarteClan(itVal, itCoul))));
         }
     }
 }
