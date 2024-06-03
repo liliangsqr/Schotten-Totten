e35bdf9db638d5da6aa9c6a154cebcb9b1060840
oussama-dghoughi <ossamadghoughi2@gmail.com>
2024-06-03 11:12:43 +0200

    Enum

---
+++ Schotten-Totten/Enum.h
@@ -0,0 +1,23 @@
+
+
+enum class Couleur {
+    Vert,Bleu,Rouge,Jaune,Marron,Violet
+};
+
+enum class CategorieCarteTactique{
+    TTroupeElite,ModeCombat,Ruse,Action
+};
+
+enum class NomCarteTactique{
+    Joker,
+    Espion,
+    PorteBouclier,
+    CollinMaillard,
+    CombatDeBoue,
+    ChasseurDeTete,
+    Stratege,
+    Banshee,
+    Traitre
+};
+
+
