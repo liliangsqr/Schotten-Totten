f9dddf6d1c37db78de357817872381a05ee82f6e
liliangsqr <lilian.gsqr@gmail.com>
2024-06-05 12:19:09 +0200

    • ajout de filtre dans les fichiers

--- Schotten-Totten/Ruse.h
+++ Schotten-Totten/Ruse.h
@@ -1,14 +1,14 @@
 #ifndef RUSE_H
 #define RUSE_H
 #include "CarteTactique.h"
-class TroupeRuse : public CarteTactique
+class Ruse : public CarteTactique
 {
 private:
 	//pNom
 	//pValeur
 	//pCouleur
 public:
-	TroupeRuse(unsigned int initValeur, unsigned int initCouleur, const string& initNom) :CarteTactique(initValeur, initCouleur, initNom) {}
+	Ruse(unsigned int initValeur, unsigned int initCouleur, const string& initNom) :CarteTactique(initValeur, initCouleur, initNom) {}
 };
 
 #endif RUSE_H
