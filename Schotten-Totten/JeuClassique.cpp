58b9cee5be9bf62e01eb040613b9db27c97d69f7
liliangsqr <lilian.gsqr@gmail.com>
2024-06-10 12:35:02 +0200

    JE COMMIT

--- Schotten-Totten/JeuClassique.cpp
+++ Schotten-Totten/JeuClassique.cpp
@@ -3,6 +3,7 @@
 void JeuClassique::initialiser(unsigned int nbJoueurs =2)// initialisation des cartes création des joueurs.
 {
 	//initialisation du tas de carte par defaut avec TotalClassqieur();
+	//initialisation des bornes dans Frontiere();
 
 	string nomJoueur;
 	for (unsigned int i = 1; i <= nbJoueurs; i++)
@@ -11,13 +12,11 @@ void JeuClassique::initialiser(unsigned int nbJoueurs =2)// initialisation des c
 		unique_ptr<Joueur> joueur = make_unique<Joueur>(nomJoueur);
 		totalJoueurs.push_back(joueur);
 	}
-
 }
 
 bool JeuClassique::terminer()
 {
-
-	
+	//aller chercher dans la map qu'il y a dans frontiere pour savoir si un joueur a gagner
 }
 
 void JeuClassique::jouerTour(Joueur& joueur)
