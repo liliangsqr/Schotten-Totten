a042c6bb9affdf14f83faa400fd0cf0cb5817e36
oussama-dghoughi <ossamadghoughi2@gmail.com>
2024-06-12 10:44:53 +0200

    Enum

--- Schotten-Totten/Borne.cpp
+++ Schotten-Totten/Borne.cpp
@@ -5,15 +5,16 @@
 bool Borne::estRevendiquableParJoueur1() const {
     Combinaison combinaisonJ1;
     for (const auto& carte : CarteJ1.getCartes()) {
-        combinaisonJ1.ajouterCarte(*carte);
+        combinaisonJ1.ajouterCarte(dynamic_cast<const CarteClan&>(*carte));
     }
     return combinaisonJ1.getMeilleureCombinaison() != CombinaisonType::Aucune;
 }
 
+
 bool Borne::estRevendiquableParJoueur2() const {
     Combinaison combinaisonJ2;
     for (const auto& carte : CarteJ2.getCartes()) {
-        combinaisonJ2.ajouterCarte(*carte);
+        combinaisonJ2.ajouterCarte(dynamic_cast<const CarteClan&>(*carte));
     }
     return combinaisonJ2.getMeilleureCombinaison() != CombinaisonType::Aucune;
 }
