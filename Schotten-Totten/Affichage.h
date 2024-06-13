ced588fa6b2fd54e94952dcbc870bf45e6abc1da
liliangsqr <lilian.gsqr@gmail.com>
2024-06-13 11:46:42 +0200

    Changements dans affichages

--- Schotten-Totten/Affichage.h
+++ Schotten-Totten/Affichage.h
@@ -4,15 +4,17 @@
 
 // Définitions des séquences d'échappement ANSI pour différentes couleurs
 
-#define RESET = "\033[0m";
-#define NOIRE = "\033[30m";
-#define ROUGE = "\033[31m";
-#define VERT = "\033[32m";
-#define JAUNE = "\033[33m";
-#define BLEU = "\033[34m";
-#define MAGENTA = "\033[35m";
-#define CYAN = "\033[36m";
-#define BLANC = "\033[37m";
+#define RESET "\033[0m";
+#define NOIRE "\033[30m";
+#define ROUGE "\033[31m";
+#define VERT "\033[32m";
+#define JAUNE "\033[33m";
+#define BLEU "\033[34m";
+#define MAGENTA "\033[35m";
+#define CYAN "\033[36m";
+#define BLANC "\033[37m";
+#define MARRON "\033[33m";
+#define VIOLET "\033[35m";
 
 class Affichage
 {
@@ -25,6 +27,7 @@ public:
 	void static carteTactique(const CarteTactique& CT);
 	void static debutJeu();
 	void static board();
+	string static Couleurs(unsigned int CarteCouelurs);
 
 
 
