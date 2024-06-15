8de97438718445d18154899eecbdba601e139baa
homerduc <alexandre.duc00@orange.fr>
2024-06-15 20:12:37 +0200

    J'ai amélioré borne, et tout ce qui concerne le joueur dans cette classe est désormais de l'accès direct avec shared_ptr

--- Schotten-Totten/Borne.cpp
+++ Schotten-Totten/Borne.cpp
@@ -1,1 +1,20 @@
 #include "Borne.h"
+
+bool Borne::tasSontTousPleins() const
+{
+    for (const auto& itMap : tasJoueurs) {
+        // Si le tas n'est pas rempli au maximum
+        if (itMap.second.GetSizeTas() != Regles::getInstance().getCapaciteMaxTasBorne()) {
+            return false;
+        }
+    }
+
+    // Tous les tas sont remplis au maximum
+    return true;
+}
+
+bool Borne::estRevendicableParJoueur(const std::string& nomJoueur) const 
+{
+    // VOIR TODO DANS LE .H
+    return tasSontTousPleins();
+}
--- Schotten-Totten/Borne.h
+++ Schotten-Totten/Borne.h
@@ -11,59 +11,69 @@
 #include "Combinaison.h"
 #include "TasDeCartes.h"
 #include "Joueur.h"
+#include "OnTop.h"
+#include "Regles.h"
 
 using namespace std;
 
 class Borne {
 private:
+    // Joueur propriétaire de la borne
+    shared_ptr<Joueur> joueurProprietaire;
+
     // Accueille les cartes Mode Combat
-    TasDeCartes<ModeCombat> OnTop;
+    OnTop onTop;
 
     // Associe le nom d'un joueur à son tas de cartes sur la borne
     map<string, TasBorne> tasJoueurs;
 
+    // Renvoie true si tous les TasBorne sont pleins, false sinon
+    bool tasSontTousPleins() const;
+
 public:
 
-    Borne() = default;
+    Borne() : joueurProprietaire(nullptr) { }
 
+    /*
     string GetInfo() const {
         string info = "Borne Info:\n";
         info += "OnTop size: " + to_string(OnTop.GetSizeTas()) + "\n";
         for (const auto& [nomJoueur, tas] : tasJoueurs) {
-            info += "Joueur " + nomJoueur + " size: " + to_string(tas.GetSize()) + "\n";
+            info += "Joueur " + nomJoueur + " size: " + to_string(tas.GetSizeTas()) + "\n";
         }
         return info;
     }
+    */
 
     // Ajoute le nom d'un joueur ainsi que son TasBorne à la borne
     void AjouterJoueur(const string& nomJoueur, TasBorne&& tasBorne) {
         tasJoueurs[nomJoueur] = move(tasBorne);
     }
 
+    // Ajoute une carte au TasBorne du joueur précisé
+    void AjouterCarteTasBorne(const Joueur& joueur, unique_ptr<Carte>&& carte) {
+        tasJoueurs[joueur.getNom()].Ajouter(std::move(carte));
+    }
+
+    // Ajoute une carte ModeCombat sur la borne
+    void AjouterCarteOnTop(unique_ptr<ModeCombat>&& carte) {
+        onTop.Ajouter(move(carte));
+    }
+
+    // TODO : même si tas adversaire pas plein prouver que victoire dans tous les cas
+    bool estRevendicableParJoueur(const std::string& nomJoueur) const;
+
+    // Rend joueur propriétaire de la borne
+    void reclamer(shared_ptr<Joueur> joueur) {
+        joueurProprietaire = joueur;
+    }
 
     const TasBorne& getTasBorne(const string& nomJoueur) const {
         return tasJoueurs.at(nomJoueur);
     }
 
-    // Ajoute une carte au TasBorne du joueur précisé
-    void AjouterCarte(const Joueur& joueur, unique_ptr<Carte>&& carte) {
-        tasJoueurs[joueur.getNom()].Ajouter(std::move(carte));
-    }
-
-    // Vérifie si un joueur a une combinaison autre que la somme
-    bool estRevendicableParJoueur(const std::string& nomJoueur) const {
-        /*
-        Combinaison combinaisonJ;
-        for (const auto& carte : tasJoueurs.at(nomJoueur).getCartes()) {
-            combinaisonJ.ajouterCarte(*carte);
-        }
-        return combinaisonJ.getMeilleureCombinaison() != CombinaisonType::Aucune;
-        */
-
-        bool retCond = true;
-        for (const auto& itMap : tasJoueurs) {
-            if (itMap.second.GetSizeTas() != )
-        }
+    shared_ptr<Joueur> getProprietaire() const {
+        return joueurProprietaire;
     }
 };
 
